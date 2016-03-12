18d0f77189f74ab199ef640b335cf69fff8a494d
Roman Mazurov <mazurov.rn@gmail.com>
2016-03-12 21:44:07 +0200

    comleated labs  2.11.1 and  2.11.2

--- src/com/brainacad/oop/testnest2_11_1/Main.java
+++ src/com/brainacad/oop/testnest2_11_1/Main.java
@@ -6,15 +6,15 @@ package com.brainacad.oop.testnest2_11_1;
  */
 public class Main {
     public static void main(String[] args) {
-//        MyPhoneBook phoneBook = new MyPhoneBook();
-//        phoneBook.addPhoneNumber("Rob", "0988252952");
-//        phoneBook.addPhoneNumber("Bob", "0988312952");
-//        phoneBook.addPhoneNumber("Joi", "0669772911");
-//        phoneBook.addPhoneNumber("Julliya", "0562222952");
-//        phoneBook.addPhoneNumber("Soffy", "0378252952");
-//        phoneBook.printPhoneBook();
-//        // Lab 2.11.2
-//        System.out.println();
+        MyPhoneBook phoneBook = new MyPhoneBook();
+        phoneBook.addPhoneNumber("Rob", "0988252952");
+        phoneBook.addPhoneNumber("Bob", "0988312952");
+        phoneBook.addPhoneNumber("Joi", "0669772911");
+        phoneBook.addPhoneNumber("Julliya", "0562222952");
+        phoneBook.addPhoneNumber("Soffy", "0378252952");
+        phoneBook.printPhoneBook();
+        // Lab 2.11.2
+        System.out.println();
 
         MyPhoneBook phoneBook1 = new MyPhoneBook();
         phoneBook1.addPhoneNumber("Rob", "0988252952");
@@ -26,9 +26,12 @@ public class Main {
         phoneBook1.addPhoneNumber("Illaya", "0578212992");
         phoneBook1.addPhoneNumber("Simon", "0378132111");
         phoneBook1.addPhoneNumber("Angelina", "0558212252");
+        phoneBook1.addPhoneNumber("Maria", "0558212252");
 
         phoneBook1.sortByName();
-//        phoneBook1.sortByPhoneNumber();
-//        phoneBook1.printPhoneBook();
+        phoneBook1.printPhoneBook();
+        System.out.println();
+        phoneBook1.sortByPhoneNumber();
+        phoneBook1.printPhoneBook();
     }
 }
